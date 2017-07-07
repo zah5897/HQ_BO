0e26dbf0d3840431d4602616b0a57e0fb9716c3d
zah <zhanah@ru-you.com>
2017-07-07 18:03:47 +0800

    版本檢測

---
+++ app/src/main/java/com/zhan/bottle/ui/activitys/AboutActivity.java
@@ -0,0 +1,20 @@
+package com.zhan.bottle.ui.activitys;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.zhan.bottle.R;
+import com.zhan.bottle.ui.base.BaseActivity;
+
+/**
+ * Created by zah on 2017/7/7.
+ */
+
+public class AboutActivity extends BaseActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        //doubleqi2016@gmail.com
+    }
+}
